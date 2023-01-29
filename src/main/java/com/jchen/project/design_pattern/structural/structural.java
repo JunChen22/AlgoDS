9d0afb19d22ffb39b96eec1f9017fa4ef39941a4
JunChen22 <ccjh522@gmail.com>
2023-01-28 20:27:48 -0500

    add design pattern

---
+++ src/main/java/com/jchen/project/design_pattern/structural/structural.java
@@ -0,0 +1,16 @@
+package com.jchen.project.design_pattern.structural;
+
+/*
+These design patterns are about organizing different classes and objects to form larger structures and provide new functionality.
+
+Structural design patterns are Adapter, Bridge, Composite, Decorator, Facade, Flyweight, Private Class Data, and Proxy.
+
+Use Case Of Structural Design Pattern-
+
+1) When 2 interfaces are not compatible with each other and want to establish a relationship between them through an adapter
+it’s called an adapter design pattern. The adapter pattern converts the interface of a class into another interface or class
+that the client expects, i.e adapter lets classes work together that could not otherwise because of incompatibility. so in these
+types of incompatible scenarios, we can go for the adapter pattern.
+ */
+public class structural {
+}
